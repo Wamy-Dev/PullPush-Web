b4b8da52bf981fd9d52656a85662c8cba17327a1
David <davidisadev@gmail.com>
2023-07-31 23:04:06 -0500

    feat: appshell header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,49 +1,43 @@
 <script lang='ts'>
-	// The ordering of these imports is critical to your app working properly
 	import '@skeletonlabs/skeleton/themes/theme-rocket.css';
-	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
-	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import Icon from '@iconify/svelte';
 </script>
-
-<!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+		<AppBar class="rounded-b-3xl">
+			<svelte:fragment slot="lead" >
+				<strong class="text-xl">PullPush.io Search</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
+					class="btn btn-sm variant-ghost-surface rounded-2xl"
+					href="https://pullpush.io/"
 					target="_blank"
 					rel="noreferrer"
 				>
-					Discord
+					<Icon icon="carbon:shape-exclude" width=24 />
 				</a>
 				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
+					class="btn btn-sm variant-ghost-surface rounded-2xl"
+					href="https://discord.gg/jsn3TSS8bs"
 					target="_blank"
 					rel="noreferrer"
 				>
-					Twitter
+					<Icon icon="carbon:logo-discord" width=24 />
 				</a>
 				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
+					class="btn btn-sm variant-ghost-surface rounded-2xl"
+					href="https://github.com/Wamy-Dev/PullPush-Web"
 					target="_blank"
 					rel="noreferrer"
 				>
-					GitHub
+					<Icon icon="carbon:logo-github" width=24/>
 				</a>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
-	<!-- Page Route Content -->
 	<slot />
 </AppShell>
