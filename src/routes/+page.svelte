5bf3659e74a1e666d7fc692bd52b3ad2794a5acf
David <davidisadev@gmail.com>
2023-08-04 05:29:56 -0500

    chore: switched icons to svgs to fix visual problem

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -200,17 +200,17 @@
 			<div>
 				{#if loading}
 					<button class="btn variant-filled rounded-3xl m-3" type="button" disabled>
-						<Icon icon="carbon:search" width=24 />
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32"><path fill="currentColor" d="m29 27.586l-7.552-7.552a11.018 11.018 0 1 0-1.414 1.414L27.586 29ZM4 13a9 9 0 1 1 9 9a9.01 9.01 0 0 1-9-9Z"/></svg>
 						<span><ProgressRadial width="w-[24px]" meter="stroke-primary-500" track="stroke-primary-500/30" /></span>
 					</button>
 				{:else}
 					<button class="btn variant-filled rounded-3xl m-3" type="submit">
-						<Icon icon="carbon:search" width=24 />
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32"><path fill="currentColor" d="m29 27.586l-7.552-7.552a11.018 11.018 0 1 0-1.414 1.414L27.586 29ZM4 13a9 9 0 1 1 9 9a9.01 9.01 0 0 1-9-9Z"/></svg>
 						<span>Search</span>
 					</button>
 				{/if}
 				<button class="btn variant-filled rounded-3xl m-3" type="reset">
-					<Icon icon="carbon:close" width=24 />
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32"><path fill="currentColor" d="M24 9.4L22.6 8L16 14.6L9.4 8L8 9.4l6.6 6.6L8 22.6L9.4 24l6.6-6.6l6.6 6.6l1.4-1.4l-6.6-6.6L24 9.4z"/></svg>
 					<span>Reset</span>
 				</button>
 			</div>
